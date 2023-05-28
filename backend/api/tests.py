82dd66b467ef0cccb8cb1881de7adbb35c35596a
Elena Vasiuchenko <39690160+svirchik@users.noreply.github.com>
2023-05-28 20:46:18 +0200

    Add tests

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,8 +1,7 @@
-from api import models
-from django.test import Client, TestCase
-
 from http import HTTPStatus
 
+from api import models
+from django.test import Client, TestCase
 
 class TaskiAPITestCase(TestCase):
     def setUp(self):
