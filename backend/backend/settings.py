552d447ae28bdc66b634f0e56e362203046008ca
Elena Vasiuchenko <39690160+svirchik@users.noreply.github.com>
2023-05-28 20:30:54 +0200

    Fix PEP8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -115,5 +115,5 @@ STATIC_ROOT = BASE_DIR / 'collected_static'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000'
+    'http://localhost:3000'
 ]
